985aedcab6db94d276445b8d5df6ca8c06f9dd0a
lah110 <lah110@brookdalecc.edu>
2021-11-03 18:34:22 -0400

    Initial commit

---
+++ library/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");//look this up
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/client", express.static(path.resolve(__dirname + "/../client/")));
+
+//Make the server
+var server;
+var port = 5000;
+
+//Page listeners (our router)
+
+//Service listeners (our data processors)
+
+//Listen
+server = app.listen(port, function(err) {
+    if(err) {
+        throw err;
+    }
+
+    console.log("Listening on port " + port);
+});
+//html for entering data and one for displaying it in a table that matches data im uploading, into client dir
